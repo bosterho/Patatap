3455e280764571c8e3dc39f15ab3b38281fb43f7
bosterho <48608512+bosterho@users.noreply.github.com>
2024-01-28 00:38:40 -0600

    Position and size animations to work for any size

--- functions.js
+++ functions.js
@@ -5,5 +5,30 @@ function triggerSound(i) {
     anim[i].play();
     soundFiles[i].playMode("restart");
     soundFiles[i].play();
-  }
-  
+}
+
+function update_animation_sizes() {
+    
+    triggerSize = windowWidth / 8
+    
+    for (let i = 0; i < numTriggers; i++) {
+        triggerX_px[i] = triggerX[i] / 100 * windowWidth
+        triggerY_px[i] = triggerY[i] / 100 * windowHeight
+    }
+    
+    let windowAspect = windowWidth / windowHeight
+    let animationAspect = 1080 / 1920
+
+    // this is not totally working
+    if (windowAspect > animationAspect) {
+        print("stretch horizontally")
+        lottie.size(windowWidth)
+        let y_offset = (lottie.size().height - 1920) / 2
+        lottie.position(0, -y_offset)
+    } else {
+        print("stretch vertically")
+        lottie.size(AUTO, windowHeight)
+        let x_offset = (lottie.size().width - 1080) / 2
+        lottie.position(-x_offset, 0)
+    }
+}
--- sketch.js
+++ sketch.js
@@ -1,154 +1,160 @@
-let numSounds = 0;
-let lottie;
-let animData = [];
-let anim = [];
-let soundFiles = [];
-let soundEntered = [];
-let triggerX = [];
-let triggerY = [];
-let triggerSize;
-let triggerAlpha = [];
-let counter = 0;
-let prev_touches;
+let numTriggers = 0
+let lottie
+let animData = []
+let anim = []
+let soundFiles = []
+let triggerEntered = []
+let triggerX = []
+let triggerX_px = []
+let triggerY = []
+let triggerY_px = []
+let triggerSize
+let triggerAlpha = []
+let counter = 0
+let prev_touches
 
-let log = "";
+let log = ""
 
-function add_sound(name, x, y) {
-  soundFiles.push(loadSound("assets/" + name + ".mp3"));
-  animData.push(loadJSON("assets/" + name + ".json"));
-  triggerX.push(windowWidth * (x / 100));
-  triggerY.push(windowHeight * (y / 100));
-  numSounds++;
+function add_trigger(name, x, y) {
+	soundFiles.push(loadSound("assets/" + name + ".mp3"))
+	animData.push(loadJSON("assets/" + name + ".json"))
+	triggerX.push(x)
+	triggerY.push(y)
+	numTriggers++
 }
 
 function preload() {
-  soundFormats("mp3");
+	soundFormats("mp3")
 
-  add_sound("strum", 25, 25);
-  add_sound("wipe", 75, 25);
-  add_sound("piano harmonic", 50, 50);
+	add_trigger("strum", 25, 25)
+	add_trigger("wipe", 75, 25)
+	add_trigger("piano harmonic", 50, 50)
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+	createCanvas(windowWidth, windowHeight)
 
-  for (let i = 0; i < numSounds; i++) {
-    triggerAlpha[i] = 5;
-  }
-  for (let i = 0; i < numSounds; i++) {
-    soundEntered[i] = [];
-  }
-  triggerSize = windowWidth / 8;
+	for (let i = 0; i < numTriggers; i++) {
+		triggerAlpha[i] = 5
+	}
+	for (let i = 0; i < numTriggers; i++) {
+		triggerEntered[i] = []
+	}
 
-  for (let i = 0; i < numSounds; i++) {
-    lottie = createDiv();
-    let params = {
-      container: lottie.elt,
-      loop: false,
-      autoplay: false,
-      animationData: animData[i],
-      renderer: "svg",
-    };
-    anim[i] = bodymovin.loadAnimation(params);
-    // lottie.position(0, 0);
-    // lottie.size(1080, 1920);
+	for (let i = 0; i < numTriggers; i++) {
+		lottie = createDiv()
+		let params = {
+			container: lottie.elt,
+			loop: false,
+			autoplay: false,
+			animationData: animData[i],
+			renderer: "svg",
+		}
+		anim[i] = bodymovin.loadAnimation(params)
 
-    anim[i].goToAndStop(1000);
-  }
+		update_animation_sizes()
+
+		anim[i].goToAndStop(1000) // to end
+	}
 }
 
 function draw() {
-  background(40);
-  noStroke();
-  for (let i = 0; i < numSounds; i++) {
-    if (triggerAlpha[i] > 5) triggerAlpha[i] -= 3;
-    fill(225, 225, 225, triggerAlpha[i]);
-    circle(triggerX[i], triggerY[i], triggerSize * 2);
-  }
-  fill(255);
-  text(log, 20, 20);
+	background(40)
+	noStroke()
+	for (let i = 0; i < numTriggers; i++) {
+		if (triggerAlpha[i] > 5) triggerAlpha[i] -= 3
+		fill(225, 225, 225, triggerAlpha[i])
+		circle(triggerX_px[i], triggerY_px[i], triggerSize * 2)
+	}
+	fill(255)
+	text(log, 20, 20)
 }
 
 function touchStarted() {
-  if (getAudioContext().state == "running") {
-    for (let i = 0; i < numSounds; i++) {
-      if (
-        dist(
-          touches[touches.length - 1].x,
-          touches[touches.length - 1].y,
-          triggerX[i],
-          triggerY[i]
-        ) < triggerSize
-      ) {
-        triggerSound(i);
-        soundEntered[i][touches.length - 1] = true;
-      }
-    }
-  }
-  userStartAudio();
+	if (getAudioContext().state == "running") {
+		for (let i = 0; i < numTriggers; i++) {
+			if (
+				dist(
+					touches[touches.length - 1].x,
+					touches[touches.length - 1].y,
+					triggerX_px[i],
+					triggerY_px[i]
+				) < triggerSize
+			) {
+				triggerSound(i)
+				triggerEntered[i][touches.length - 1] = true
+			}
+		}
+	}
+	userStartAudio()
 }
 
 function mousePressed() {
-  if (getAudioContext().state == "running") {
-    for (let i = 0; i < numSounds; i++) {
-      if (dist(mouseX, mouseY, triggerX[i], triggerY[i]) < triggerSize) {
-        triggerSound(i);
-        soundEntered[i][0] = true;
-      }
-    }
-  }
-  userStartAudio();
+	if (getAudioContext().state == "running") {
+		for (let i = 0; i < numTriggers; i++) {
+			if (dist(mouseX, mouseY, triggerX_px[i], triggerY_px[i]) < triggerSize) {
+				triggerSound(i)
+				triggerEntered[i][0] = true
+			}
+		}
+	}
+	userStartAudio()
 }
 
 function touchMoved() {
-  for (let i = 0; i < numSounds; i++) {
-    for (let t = 0; t < touches.length; t++) {
-      if (
-        dist(touches[t].x, touches[t].y, triggerX[i], triggerY[i]) < triggerSize
-      ) {
-        if (!soundEntered[i][t]) {
-          triggerSound(i);
-        }
-        soundEntered[i][t] = true;
-      } else {
-        soundEntered[i][t] = false;
-      }
-    }
-  }
-  return false;
+	for (let i = 0; i < numTriggers; i++) {
+		for (let t = 0; t < touches.length; t++) {
+			if (
+				dist(touches[t].x, touches[t].y, triggerX_px[i], triggerY_px[i]) < triggerSize
+			) {
+				if (!triggerEntered[i][t]) {
+					triggerSound(i)
+				}
+				triggerEntered[i][t] = true
+			} else {
+				triggerEntered[i][t] = false
+			}
+		}
+	}
+	return false
 }
 
 function mouseDragged(event) {
-  for (let i = 0; i < numSounds; i++) {
-    if (dist(mouseX, mouseY, triggerX[i], triggerY[i]) < triggerSize) {
-      if (!soundEntered[i][0]) {
-        triggerSound(i);
-      }
-      soundEntered[i][0] = true;
-    } else {
-      soundEntered[i][0] = false;
-    }
-  }
+	for (let i = 0; i < numTriggers; i++) {
+		if (dist(mouseX, mouseY, triggerX_px[i], triggerY_px[i]) < triggerSize) {
+			if (!triggerEntered[i][0]) {
+				triggerSound(i)
+			}
+			triggerEntered[i][0] = true
+		} else {
+			triggerEntered[i][0] = false
+		}
+	}
 }
 
 function keyPressed() {
-  // a
-  if (keyCode == 65) {
-    triggerSound(0);
-  }
+	// a
+	if (keyCode == 65) {
+		triggerSound(0)
+	}
 
-  // s
-  if (keyCode == 83) {
-    triggerSound(1);
-  }
+	// s
+	if (keyCode == 83) {
+		triggerSound(1)
+	}
 
-  // d
-  if (keyCode == 68) {
-    triggerSound(2);
-  }
+	// d
+	if (keyCode == 68) {
+		triggerSound(2)
+	}
 
-  // f
-  if (keyCode == 70) {
-    triggerSound(3);
-  }
+	// f
+	if (keyCode == 70) {
+		triggerSound(3)
+	}
+}
+
+function windowResized() {
+	resizeCanvas(windowWidth, windowHeight);
+	update_animation_sizes()
 }
